17f95c42a5604fa7eeb08912fed10c1a1f4fcd24
cexoso <285516193@qq.com>
2015-07-22 18:34:38 +0800

    nd

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,18 +6,43 @@ angular
     'controller'
   ])
   .config(function($stateProvider, $urlRouterProvider) {
-     $urlRouterProvider.otherwise('/index');
-     $stateProvider.state('index', {
-        url: '/index',
-        views: {
-          '': {
-            templateUrl: 'views/index.html',
-            controller:'indexController'
-          } 
+     $urlRouterProvider.otherwise('/b/b');
+     // $stateProvider.state('index', {
+     //    url: '/index',
+     //    views: {
+     //      '': {
+     //        templateUrl: 'views/index.html',
+     //        controller:'indexController'
+     //      } 
+     //    }
+     //  })
+     $stateProvider.state('a ', {
+        url:'/a',
+        templateUrl: 'views/index.html'
+      }).
+     state('ab',{
+        url:'/b/:id',
+        templateUrl: 'views/customerService.html',
+        controller:function($stateParams){
+          console.log($stateParams)
         }
-      })
+     })
   });
 
+
+// angular.module('paticaApp').config(['$stateProvider','$urlRouterProvider',function($stateProvider, $urlRouterProvider) {
+//      $stateProvider.state('a ', {
+//         url:'/a',
+//         templateUrl: 'views/index.html'
+//       }).
+//      state('a.b',{
+//         url:'/b',
+//         templateUrl: 'views/customerService.html'
+//      })
+//   }])
+
 angular.module('controller',[]);
+
+
 
 // http://www.patica.com.cn/patica/html/index.html?openid=ohQRxs4EMc4dBVahHIY7HvvQ2XNM&user_access_token=OezXcEiiBSKSxW0eoylIeKgL0Csc5Fk-ftXOSB6u_ubDy-sXqSiiI98l2784Y5uMrHrNF4W4zBeG4aPxU3kbFG502-quHHSFlDR2c72MYLpe5fJXcBYLKcPMTQyF9uWJPZeSDcDnYrz0EB9rrukY6Q
