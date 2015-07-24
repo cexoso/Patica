429d09cd1228a47a16608d4592580ac7952709d7
cexoso <285516193@qq.com>
2015-07-24 11:47:07 +0800

    第一个页面完成

--- app/scripts/controllers/orderController.js
+++ app/scripts/controllers/orderController.js
@@ -7,149 +7,66 @@ controller('orderController',['$scope','$resource',function(s,resource){
             return orderInfo.get({name:name,id:id});
         }
     })();
-    s.show="brand";
-    s.step="1";
+    s.show="brand"; 
+    //一次只能显示一个 {品牌|型号|颜色|故障}
+    s.step=1;
+    //  第1步 品牌
+    //  第2步 型号
+    //  第1步 颜色
+    //  第1步 故障
     s.oi={};
+    //用来保存订单信息的
+    //oi.brand='apple' oi.version='iPhone 6 plus'...
     var list=s.list={};
-    list.brandType=loadData('brandList',123);
-    list.versionType=loadData('versionList',123);
-    list.colorType=loadData('colorList',123);
-    list.colorType=loadData('faultList',123);
-    var brand = {
-               
-            },
-            version = {
-                name: "型号",
-                check: false,
-                show: false
-            },
-            color = {
-                name: "颜色",
-                check: false,
-                show: false
-            },
-            fault = {
-                name: "故障",
-                check: false,
-                show: false
-            };
-        s.brand = brand;
-        s.brandList = [
-            "苹果"
-        ];
-        s.version = version;
-        s.versionList = [
-            "Iphone4",
-            "Iphone4s",
-            "Iphone5",
-            "Iphone5c",
-            "Iphone5s",
-            "Iphone6",
-            "Iphone6 plus"
-        ];
-        s.color = color;
-        s.colorList = [
-            "金色",
-            "灰色",
-            "白色"
-        ];
-        s.fault = fault;
-        s.faultList = [
-            "摄像头",
-            "屏幕显示不正常",
-            "数据线插口",
-            "外壳",
-            "信号弱",
-            "蓝牙故障",
-           "耳机孔",
-            "扬声器",
-            "听筒",
-            "按键失灵",
-            "无线连接",
-            "电池",
-            "感应功能",
-            "进水",
-            "其他"
-           
-        ];
-        s.brandClickHandle = function() {
-            s.brand.show = !s.brand.show;
+    //list 用于页面展现数据
+    list.brandType=loadData('brandList');
+    /**
+     * 用于品牌型号颜色故障的数据选取处理
+     */
+    var handle=(function(){
+        var step=[
+            "brand",
+            "version",
+            "color",
+            "fault"
+        ],
+        oi=s.oi;
+        function checkHandle(key,value_obj){
+            // key 代表你要修改的键 brand version color fault
+            // value_obj 代表你想要改成什么 brand='apple'
+            s.step=step.indexOf(key)+2;
+            //显示当前步成功
+            s.show=step[step.indexOf(key)+1];
+            //显示下一步
+            var nextType=step[step.indexOf(key)+1];
+            if(nextType){
+                //如果有下一项
+                list[nextType+'Type']=loadData(nextType+'List',value_obj.id);
+                //加载下一项数据
+            }
+            //清空之后信息 brand选择之后 version 清空
+            for(var i=step.indexOf(key)+1;i<step.length;i++){
+                delete oi[step[i]];
+            }
+            oi[key]=value_obj;
+            //在页面上显示当前选择的值
         }
-        s.versionClickHandle = function() {
-            if (s.brand.check) {
-                s.version.show = !s.version.show;
-            } else {
-                s.version.show = false;
+        function clickHandle(key){
+            if(s.step>=step.indexOf(key)+1){
+                s.show=(s.show==key?"":key)
             }
         }
-        s.colorClickHandle = function() {
-            if (s.version.check) {
-                s.color.show = !s.color.show;
-            } else {
-                s.color.show = false;
-            }
+        return {
+            checkHandle:checkHandle,
+            clickHandle:clickHandle,     
         }
-        s.faultClickHandle = function() {
-            if (s.color.check) {
-                s.fault.show = !s.fault.show;
-            } else {
-                s.fault.show = false;
-            }
-
+    })();
+    s.checkHandle=handle.checkHandle;
+    s.clickHandle=handle.clickHandle;
+    s.submit=function(){
+        if(s.step>4){
+        }else{
+            console.log('信息不完全');
         }
-
-        s.checkBrand = function(name) {
-            s.brand.name = name;
-            s.brand.show = false;
-            s.brand.check = true;
-            s.version = {
-                name: '型号',
-                check: false,
-                show: true
-            };
-            s.color = {
-                name: "颜色",
-                check: false,
-                show: false
-            };
-            s.fault = {
-                name: "故障",
-                check: false,
-                show: false
-            }
-        }
-        s.checkVersion = function(name) {
-            s.version.name = name;
-            s.version.show = false;
-            s.version.check = true;
-
-            s.color = {
-                name: "颜色",
-                check: false,
-                show: true
-            };
-            s.fault = {
-                name: "故障",
-                check: false,
-                show: false
-            };
-        }
-        s.checkColor = function(name) {
-            s.color.name = name;
-            s.color.show = false;
-            s.color.check = true;
-
-            s.fault = {
-                name: "故障",
-                check: false,
-                show: true
-            }
-        }
-
-        s.checkfault=function(name){
-            s.fault.name = name;
-            s.fault.show = false;
-            s.fault.check = true;
-        }
-
+    }
 }]);
